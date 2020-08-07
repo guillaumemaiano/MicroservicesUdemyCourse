5450c1e79e08418aa1a81e86d12ae9d7156937e6
GEM <guillaume@maiano.fr>
2020-08-07 12:17:27 +0200

    Naive implementation of query service done

--- part1/blog/query/index.js
+++ part1/blog/query/index.js
@@ -8,20 +8,34 @@ app.use(parser.json());
 app.use(cors());
 
 const queryPort = 4002;
+const eventbusPort = 4005;
 const pathToPosts='/posts';
 const pathToEvents= '/events';
 
+// not likely the most efficient structure in any way except quick coding
 const posts = [];
+const comments = [];
 
 // provides full list of posts and comments
 app.get(pathToPosts, (req, res) => {
     
-    res.send({status: 'OK', data: posts});
+    res.send({status: 'OK', data: {"posts": posts, "comments": comments}});
 });
 
 // event bus communication
 app.post(pathToEvents, (req, res) => {
-    //switch req.body.type 
+    switch (req.body.type) {
+        case 'CommentCreated':
+            console.log('Comment Creation detected.');
+            posts.push({id: req.body.data.randomId, content: req.body.data.content});   
+            break;
+        case 'PostCreated':
+            console.log('Post Creation detected.');
+            posts.push({id: req.body.data.randomId, content: req.body.data.content});          
+            break;
+        default:
+            console.log('Type unknown, event bus format change unimplemented: ' + req.body.type);
+    }
 
 });
 
