f0137449cb236915d86edc77ad31b64f2ec1a08d
GEM <guillaume@maiano.fr>
2020-08-07 16:28:20 +0200

    Improves the data structure

--- part1/blog/query/index.js
+++ part1/blog/query/index.js
@@ -8,33 +8,64 @@ app.use(parser.json());
 app.use(cors());
 
 const queryPort = 4002;
-const eventbusPort = 4005;
 const pathToPosts='/posts';
 const pathToEvents= '/events';
 
-// not likely the most efficient structure in any way except quick coding
 const posts = [];
-const comments = [];
+
+const verbose = (() => {
+    const arguments = process.argv;
+    arguments.forEach(function (val, index, arguments) {
+        console.log(index + ': ' + val);
+        if (val === 'verbose') {
+            console.log("Talkative mode engaged. How do you do?")
+            return true;
+        }
+    });
+    return false;
+    }
+)();
 
 // provides full list of posts and comments
-app.get(pathToPosts, (req, res) => {
-    
-    res.send({status: 'OK', data: {"posts": posts, "comments": comments}});
+app.get(pathToPosts, (req, res) => {  
+    res.send({status: 'OK', data: {"posts": posts}});
 });
 
 // event bus communication
 app.post(pathToEvents, (req, res) => {
     switch (req.body.type) {
         case 'CommentCreated':
-            console.log('Comment Creation detected.');
-            posts.push({id: req.body.data.randomId, postId, content: req.body.data.content});   
+            if (verbose) {
+                console.log('Comment Creation detected.');
+            }
+            // data.postId is the comment's parent's id
+            const postId = req.body.data.postId;
+            // parentPost must exist at this stage (since someone commented on it)
+            const parentPost = ((postArray, id) => {
+                for (var i = 0; i < postArray.length; i++) {
+                    if (postArray[i].id === id) {
+                        return postArray[i];
+                    }
+                    console.log("Coherence error: Post not found.");
+                }
+            })(posts, postId);
+            const commentsForPost = parentPost.comments || [];
+            // data.id is the comment's id
+            const commentId = req.body.data.id;
+            commentsForPost.push({id: commentId, content: req.body.data.content});
+            parentPost.comments = commentsForPost;
+            // is post modified in place?
+            console.log(posts, " Comments ", commentsForPost);
             break;
         case 'PostCreated':
-            console.log('Post Creation detected.');
-            posts.push({id: req.body.data.randomId, content: req.body.data.content});          
+            if (verbose) {
+                console.log('Post Creation detected.');
+            }
+            posts.push({id: req.body.data.randomId, content: req.body.data.title});      
             break;
         default:
             console.log('Type unknown, event bus format change unimplemented: ' + req.body.type);
+        res.send(201);
     }
 
 });
