7f8b244cc0819429bc0bd80ce7e7ec5cff3c18d4
GEM <guillaume@maiano.fr>
2020-08-13 17:07:43 +0200

    UI done... for a techie

--- part1/blog/client/src/CommentList.js
+++ part1/blog/client/src/CommentList.js
@@ -1,17 +1,35 @@
 import React from 'react';
 
-export default({ comments }) => {
+export default ({ comments }) => {
+
+    const statusOfComment = (status) => {
+        switch (status) {
+            case "pending":
+                return <div className='unmoderated'>"This comment is awaiting moderation."</div>
+            case "rejected":
+                return  <div className='rejected'>
+                    "This comment was rejected"
+                </div>
+            case "approved":
+            // no action required
+                break;
+            default:
+                return <div className="error">
+                    {status}
+                </div>;
+        }
+    };
 
     const renderedComments = comments
-                                .map(
-                                    comment => {
-                                        return <li className='comment' key={comment.id}>
-                                            {comment.content }
-                                            {comment.status}
-                                        </li>;
-                                    }
-                                );
- 
+        .map(
+            comment => {
+                return <li className='comment' key={comment.id}>
+                    {comment.content}
+                    {statusOfComment(comment.status)}
+                </li>;
+            }
+        );
+
     return <ul className="justify-content-between d-flex flex-right flex-wrap">
         {renderedComments}
     </ul>;
